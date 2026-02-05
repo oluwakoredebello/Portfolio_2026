076164e074b4549f25ba177d5636daa47bf1af27
Oluwakorede Bello <257461695+oluwakoredebello@users.noreply.github.com>
2026-02-05 20:27:53 +0000

    feat: identified revenue leakage in legacy system

--- fintech_audit/models/fct_gateway_audit.sql
+++ fintech_audit/models/fct_gateway_audit.sql
@@ -1,6 +1,15 @@
--- with staging as (
---     select * from {{ ref('stg_subscriptions') }}
--- )
+with staging as (
+    select * from {{ ref('stg_subscriptions') }}
+)
 
--- select 
---     gateway_name,
+select 
+    payment_gateway,
+    count(user_id) as total_customers,
+    sum(potential_revenue) as expected_revenue,
+    sum(potential_revenue * is_leaked_revenue_flag) as total_dollars_leaked,
+    round(
+        (cast(sum(potential_revenue * is_leaked_revenue_flag) / nullif(sum(potential_revenue), 0) as float) * 100), 
+     2) as leakage_pct
+from staging
+group by 1
+order by total_dollars_leaked desc
