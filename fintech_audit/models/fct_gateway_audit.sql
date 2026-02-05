407f660428858cf60b621e059821ea64a743cfb5
Oluwakorede Bello <257461695+oluwakoredebello@users.noreply.github.com>
2026-02-05 22:31:58 +0000

    feat: identified churn rate in inactive customer behavior

--- fintech_audit/models/fct_gateway_audit.sql
+++ fintech_audit/models/fct_gateway_audit.sql
@@ -8,8 +8,8 @@ select
     sum(potential_revenue) as expected_revenue,
     sum(potential_revenue * is_leaked_revenue_flag) as total_dollars_leaked,
     round(
-        (cast(sum(potential_revenue * is_leaked_revenue_flag) / nullif(sum(potential_revenue), 0) as float) * 100), 
-     2) as leakage_pct
+        (cast(sum(potential_revenue * is_leaked_revenue_flag) / nullif(sum(potential_revenue), 0) as float) * 100),
+        2) as leakage_pct
 from staging
 group by 1
 order by total_dollars_leaked desc
---
+++ fintech_audit/models/fct_retention_audit.sql
@@ -0,0 +1,23 @@
+with staging as (
+    select * from {{ ref('stg_subscriptions') }}
+)
+
+-- are people with high inactivity actually more likely to be 'Cancelled' then 'Active?
+
+select
+    is_inactive_user_flag,
+
+    count(user_id) as total_customers,
+    sum(case
+            when payment_status = 'Cancelled' then 1
+            else 0
+        end) as total_cancellations,
+    round(
+        (sum(case
+                when payment_status = 'Cancelled' then 1
+                else 0
+            end) * 100.0) / nullif(count(user_id), 0), 2
+    ) as churn_rate_pct
+from staging
+group by 1
+order by is_inactive_user_flag desc
