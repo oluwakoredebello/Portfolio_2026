24a6d4a6aa71bcf0f7f1a41655908d8abe2df4cb
Oluwakorede Bello <257461695+oluwakoredebello@users.noreply.github.com>
2026-02-09 02:03:26 -0500

    feat: update ingestion pipeline to incorporate functions

--- fintech_audit.py
+++ fintech_audit.py
@@ -22,6 +22,34 @@ legacy_weights = [0.05, 0.05, 0.85, 0.025, 0.025]
 growth_weights = [0.05, 0.05, 0.60, 0.20, 0.10]
 modern_weights = [0.10, 0.10, 0.05, 0.40, 0.35]
 
+#tenure determines gateway choice, gateway choice influences status_choice, login_gap_days increases cancellations
+#defining status logic and assignment logic
+def select_status(gateway_choice, login_gap_days):
+    if gateway_choice == 'Legacy_Internal_System':
+        status_choice = random.choices(status_options, weights = failed_weights)[0]
+        return status_choice
+    
+    if login_gap_days > 30:
+        status_choice = random.choices(status_options, weights = cancellation_weights)[0]
+        return status_choice
+    
+    else:
+        status_choice = random.choices(status_options, weights = healthy_weights)[0]
+        return status_choice
+
+def assignment_logic(tenure, login_gap_days):
+    if tenure > 15: #simulating increased chance for a consumer to be assigned to Legacy Payment Gateway if tenure is higher than 15
+        gateway_choice = random.choices(gateway_options, weights = legacy_weights)[0]
+
+    elif tenure <= 5: #simulating increased chance for a consumer to be assigned to Modern Payment Gateways if tenure is less than or equal to 5
+        gateway_choice = random.choices(gateway_options, weights = modern_weights)[0] 
+        
+    else: #simulating moderate chances for a consumer to be assigned to Legacy and Modern Gateways
+        gateway_choice = random.choices(gateway_options, weights = growth_weights)[0] 
+
+    status_choice = select_status(gateway_choice, login_gap_days)
+    return gateway_choice, status_choice
+  
 
 # creating a randomized large-volume datase, utilizing Faker and random modules
 for i in range(1_500_000):
@@ -29,42 +57,8 @@ for i in range(1_500_000):
     years_with_the_firm = random.randint(0, 25)
     login_gap_days = random.randint(0, 90)
     plan_choice = random.choices(plan_options, weights = plan_weights)[0]
+    gateway_choice, status_choice = assignment_logic(years_with_the_firm, login_gap_days)
 
-    if years_with_the_firm > 15: #simulating increased chance for a consumer to be assigned to Legacy Payment Gateway if tenure is higher than 15
-        gateway_choice = random.choices(gateway_options, weights = legacy_weights)[0]
-        if gateway_choice == 'Legacy_Internal_System':
-            status_choice = random.choices(status_options, weights = failed_weights)[0]
-
-        elif login_gap_days > 30:
-            status_choice = random.choices(status_options, weights = cancellation_weights)[0]
-
-        else:
-            status_choice = random.choices(status_options, weights = healthy_weights)[0]
-
-    elif years_with_the_firm <= 5: #simulating increased chance for a consumer to be assigned to Modern Payment Gateways if tenure is less than 5
-        gateway_choice = random.choices(gateway_options, weights = modern_weights)[0] 
-        if gateway_choice == 'Legacy_Internal_System':
-            status_choice = random.choices(status_options, weights = failed_weights)[0]
-
-        elif login_gap_days > 30:
-            status_choice = random.choices(status_options, weights = cancellation_weights)[0]
-
-        else:
-            status_choice = random.choices(status_options, weights = healthy_weights)[0]
-    
-    else: #simulating moderate chances for a consumer to be assigned to Legacy and Modern Gateways
-        gateway_choice = random.choices(gateway_options, weights = growth_weights)[0] 
-        if gateway_choice == 'Legacy_Internal_System':
-            status_choice = random.choices(status_options, weights = failed_weights)[0]
-
-        elif login_gap_days > 30:
-            status_choice = random.choices(status_options, weights = cancellation_weights)[0]
-
-        else:
-            status_choice = random.choices(status_options, weights = healthy_weights)[0]
-
-
-    
     item = {
         'user_id': i,
         'user_name': fake.name(),
